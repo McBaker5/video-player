727b737b9f88d427d17de1edd75212ca1d32c279
McBaker5 <jackson@bakerplace.org>
2021-10-08 15:31:59 -0500

    fix clientApp.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
 import ReactPlayer from 'react-player'
+import firebase from '../firebase/clientApp'
+
 
 export default function Home() {
   return (
