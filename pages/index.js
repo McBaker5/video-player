790669b8330f0ca8be054da4e42aa4c7c9ef2687
Brinden Robinson <sevynz@gmail.com>
2021-10-10 15:53:50 -0500

    Added out of the box basic recording capability

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,28 @@ import styles from '../styles/Home.module.css'
 import React from 'react'
 import ReactPlayer from 'react-player'
 import firebase from '../firebase/clientApp'
-
-
+import { render } from 'react-dom'
+import VideoRecorder from 'react-video-recorder'
 export default function Home() {
   return (
-    <div>
-      <ReactPlayer url='https://www.youtube.com/watch?v=vwJbALuO-pM' />
-    </div>
+    <VideoRecorder
+    rederActions={actionButton => {
+        <actionButton>d</actionButton>
+    }}
+    // onRecordingComplete={videoBlob => {
+
+    //   console.log('videoBlob', videoBlob)
+      
+    // }}
+  />
+  
   )
 }
+
+// export async function getServerSideProps(context) {
+//   return{
+//    // props: {rederActions},
+//   }
+// }
+
+// render(<Home />, document.getElementById('root'))
