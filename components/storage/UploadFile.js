7520329e9482727ff95bf0b2d9b56e99aca1a2c2
McBaker5 <jackson@bakerplace.org>
2021-10-10 14:32:08 -0500

    create document in firestore
    Create a document in the videos collection and add the download URL of the uploaded video as an attribute

--- components/storage/UploadFile.js
+++ components/storage/UploadFile.js
@@ -4,13 +4,31 @@ import firebase from '../../firebase/clientApp'
 const UploadFile = () => {
     const inputFile = useRef(null)
 
-    function uploadFile() {
+    const uploadFile = async () => {
         // get file
         var file = inputFile.current.files[0]
         // create a storage ref to videos directory
         var storageRef = firebase.storage().ref('videos/' + file.name)
         // upload file
-        storageRef.put(file)
+        await storageRef.put(file)
+
+
+        // Add to firestore
+        storageRef.getDownloadURL().then((url) => {
+            var docRef = firebase.firestore().collection('videos').doc() // leave as .doc() for a random unique doc name to be assigned
+            docRef.set({
+                description: 'testing firestore document creation when a video is uploaded',
+                enabled: false,
+                videoPath: url,
+                ID: docRef.id,
+                likeCount: 0,
+                createdAt: firebase.firestore.Timestamp.now(),
+                macroTagIDs: [],
+                microTagIDs: [],
+                title: 'Test'
+            })
+            .then(alert('Data was successfully sent to firestore'))
+        })
     }
 
     return (
