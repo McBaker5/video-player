3a31c22708df5e79058c1c1911c6c469bc8b3389
Brinden Robinson <sevynz@gmail.com>
2021-10-10 22:36:53 -0500

    Added video saving support to firebase, as well as set up video recording environment to simulate the user video recording experience

--- pages/recorder.js
+++ pages/recorder.js
@@ -1,13 +1,23 @@
 import VideoRecorder from 'react-video-recorder'
 import React from 'react'
+import { render } from 'react-dom'
+import UploadFile from '../components/storage/UploadFile'
+import Upload from './upload'
+import firebase from '../firebase/clientApp'
+import { collection, addDoc } from "firebase/firestore"; 
 
-export default function Recorder({video}) {
+export default function Recorder({docRef}) {
     return (
-      <VideoRecorder
+      
+      <VideoRecorder timeLimit={60000} countdownTime={0} // mimeType={[".mp4",".hls",".mov"]}
       onRecordingComplete={(videoBlob) => {
-        console.log('videoBlob', videoBlob)
+        const db = firebase.firestore();
+        // Create a reference to a document in the videos collection
+        var Ref = db.collection("uploads").doc("Q2TUSOl3xY2aB6jwVyz1")
+        const docre =  addDoc(collection(Ref,"Video"),{
+          Video:firebase.firestore.Blob.fromUint8Array(videoBlob)
+        });
       }}
     />
-    
-    )
-  }
+    );
+  };
