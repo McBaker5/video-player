658a57e33f153ba366331144ac15a071a6bd1527
McBaker5 <jackson@bakerplace.org>
2021-10-11 00:13:57 -0500

    Update recorder.js
    Used code from UploadFile.js to upload the video to storage and database.

--- pages/recorder.js
+++ pages/recorder.js
@@ -4,19 +4,31 @@ import { render } from 'react-dom'
 import UploadFile from '../components/storage/UploadFile'
 import Upload from './upload'
 import firebase from '../firebase/clientApp'
-import { collection, addDoc } from "firebase/firestore"; 
 
 export default function Recorder({docRef}) {
     return (
-      
       <VideoRecorder timeLimit={60000} countdownTime={0} // mimeType={[".mp4",".hls",".mov"]}
-      onRecordingComplete={(videoBlob) => {
-        const db = firebase.firestore();
-        // Create a reference to a document in the videos collection
-        var Ref = db.collection("uploads").doc("Q2TUSOl3xY2aB6jwVyz1")
-        const docre =  addDoc(collection(Ref,"Video"),{
-          Video:firebase.firestore.Blob.fromUint8Array(videoBlob)
-        });
+      onRecordingComplete={videoBlob => {
+        //uploadToStorage(videoBlob)
+        var storageRef = firebase.storage().ref('videos/' + 'test')
+        // upload file
+        storageRef.put(videoBlob)
+        // Add to firestore
+        storageRef.getDownloadURL().then((url) => {
+          var docRef = firebase.firestore().collection('videos').doc() // leave as .doc() for a random unique doc name to be assigned
+          docRef.set({
+              description: 'testing firestore document creation when a video is uploaded',
+              enabled: false,
+              videoPath: url,
+              ID: docRef.id,
+              likeCount: 0,
+              createdAt: firebase.firestore.Timestamp.now(),
+              macroTagIDs: [],
+              microTagIDs: [],
+              title: 'Test'
+          })
+          .then(alert('Data was successfully sent to firestore'))
+        })
       }}
     />
     );
